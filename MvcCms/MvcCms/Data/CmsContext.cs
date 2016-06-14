7f772e5e81321191a9caedc3e58e124475fa4b5d
CashmereBob <weidmar@gmail.com>
2016-06-14 14:07:03 +0200

    Migrations done and working

---
+++ MvcCms/MvcCms/Data/CmsContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MvcCms.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcCms.Data
+{
+    public class CmsContext : IdentityDbContext<CmsUser>
+    {
+        public CmsContext() : base("CmsContextConnectionString") { }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Post>()
+                .HasKey(e => e.Id)
+                .Property(e => e.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+
+            modelBuilder.Entity<Post>()
+                .HasRequired(e => e.Author);
+        }
+    }
+}
